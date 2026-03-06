dac4db303105c60953561584cd01c07ad23ff577
Matthew Miller <millerdimarem@gmail.com>
2026-03-05 21:23:56 -0600

    prep for app store

---
+++ app/src/main/java/io/github/mtage70/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.mtage70.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
